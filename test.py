23f0ef315090d9d8ae5f57e21da6ae498cf6fcf1
asimakiskydros <asimakis@csd.auth.gr>
2025-01-29 18:26:22 +0200

    Turned how plotting works into a context manager so colors can more easily and clearly be carried over after shift.

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from qiskit import QuantumCircuit, QuantumRegister, transpile
 from qiskit_aer import AerSimulator
-from typing import Callable, Literal
+from typing import Callable, Literal, Sequence, Mapping
 from itertools import product
 from math import log2
 from textwrap import wrap
 import matplotlib.pyplot as plt
 
 
-def load_shape (dimensions: int, size: int, rule: Callable, show = False):
+def encoding (dimensions: int, size: int, rule: Callable):
     """
     Maps the given rule into a quantum binary image of given `dimensions` and given `size` per dimension.
     """
@@ -31,88 +31,29 @@ def load_shape (dimensions: int, size: int, rule: Callable, show = False):
         if rule(*point):
             control_state = ''.join([bin(coordinate)[2:].zfill(qubits) for coordinate in reversed(point)])
             circuit.mcx(controls, col, ctrl_state=control_state)
-
-    if show:
-        circuit.draw('mpl')
-        plt.show()
     
-    return circuit, pos, col
+    return circuit, pos
 
 
-def probability_vector (circuit: QuantumCircuit, shots = 10_000, condition: Literal['0', '1'] = '1'):
+def statevector (circuit: QuantumCircuit, shots = 10_000, condition: Literal['0', '1'] = '1') -> list[str]:
     """
     Measures all qubits and returns the quasi-probability vector calculated from the resulting counts.
     """
-    # NOTE: My dumbass thought that I could use Statevector(...) instead of this entire routine 
-    # HAHA! v1.3.1 it yields garbage for size=4 and size=8, v.1.2.4 yields garbage only for size=8
-    # This works on all versions for all sizes. 
     circuit.measure_all()
     
     backend = AerSimulator()
-    counts: dict[str, int] = backend.run(transpile(circuit, backend), shots=shots).result().get_counts()
-    counts = { state[1:]: hits for state, hits in counts.items() if state[0] == condition }
-    norm = sum(hits for hits in counts.values())
+    counts: Mapping[str, int] = backend.run(transpile(circuit, backend), shots=shots).result().get_counts()
 
     circuit.remove_final_measurements()
 
-    return { state: hits / norm for state, hits in counts.items() }
-
-
-def plot (dimensions: int, circuit: QuantumCircuit, show = False, highlight: tuple[str] = None):
-    """
-    Plots the given circuit in the asked dimensions.
-    """
-    if dimensions not in (2, 3): 
-        raise NotImplementedError(f'Cannot plot {dimensions}-dimensional plot yet.')
-    
-    probs = probability_vector(circuit)
-
-    # split basis states back into the coordinates they represent
-    # and add them to the appropriate dimensional sublist
-    coords = [[] for _ in range(dimensions)]
-    hcoords = None
-    for state in probs.keys():
-        point = [int(dim, 2) for dim in wrap(state, (circuit.num_qubits - 1) // dimensions)]
-        if highlight is not None and point == highlight:
-            hcoords = point
-            continue
-
-        for sublist, dim in zip(coords, point, strict=True):
-            sublist.append(dim)
-
-    if dimensions == 3:
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-    else:
-        fig, ax = plt.subplots()
-
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-
-    if dimensions == 3:
-        ax.set_zlabel('z')
-
-    ax.scatter(*reversed(coords), c='blue')
-    
-    if hcoords:
-        ax.scatter(*reversed(hcoords), c='red', marker='x')
-        ax.text(
-            *(c + 0.2 for c in reversed(hcoords)), 
-            f'({", ".join(str(c) for c in reversed(hcoords))})', color='red')
-    
-    if show:
-        plt.show()
-
-    return fig
+    return [state[1:] for state in counts.keys() if state[0] == condition]
 
 
 def cycle_shift (circuit: QuantumCircuit, pos: QuantumRegister, step: int = 1, show = False, barrier = True):
     """
     Shifts mod 2**len(pos) along the dimension expressed by `pos`, for the given amount of `step`s.
     """
-    if barrier:
-        circuit.barrier()
-
+    if barrier: circuit.barrier()
     # can only truly shift by mod 2**n jumps, so normalize the step to this region
     # if the true jump is 0 steps, do nothing
     if (_step := abs(step) % 2 ** pos.size) == 0: 
@@ -139,28 +80,99 @@ def cycle_shift (circuit: QuantumCircuit, pos: QuantumRegister, step: int = 1, s
     cycle_shift(circuit, pos, (_step - largest_p2) * (1 if step > 0 else -1), barrier=False)
 
 
+class Plotter:
+    """
+    Custom context manager to handle shifting and plotting more clearly
+    """
+    def __init__(self, circuit: QuantumCircuit, axes: Sequence[QuantumRegister]):
+        if (dims := len(axes)) not in (2, 3):
+            raise NotImplementedError(f'Cannot plot {dims}D structures.')
+
+        self.circ = circuit
+        self.axes = axes
+        self.apriori = statevector(self.circ)  # keep a record of the original image
+        self.a_colors = { state: i for i, state in enumerate(self.explode(self.apriori)) }  # og image color/point
+        self.p_colors: dict[tuple[int], int] = self.a_colors.copy()  # shifted image color/point
+        
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        posteriori = statevector(self.circ)  # snapshot the shifted image
+        self.plot(self.apriori, self.a_colors, 'Before')  # plot original
+        self.plot(posteriori, self.p_colors, 'After')  # plot shifted
+        self.circ.draw('mpl')  # plot circuit
+        plt.show()
+
+    def explode (self, states: Sequence[str]):
+        """
+        Maps the basis states into the points they represent as integer tuples.
+        """
+        chunk = (self.circ.num_qubits - 1) // len(self.axes)
+
+        for state in states:
+            yield tuple(int(dim, 2) for dim in wrap(state, chunk))
+
+    def shift (self, axis: Sequence[int], step: int = 1):
+        """
+        Convenience function to be used inside the context manager. Allows declaring the entire shift-axis at once
+        and how many steps along its path to take.
+        """
+        for dim, scalar in enumerate(axis):
+            cycle_shift(self.circ, self.axes[dim], step * scalar)
+
+        updated = self.p_colors.copy()
+        overflow = 1 << self.axes[0].size
+
+        for point, value in self.p_colors.items():
+            # update the shifted color/point record by updating the keys but keeping the colors the same
+            new_point = tuple((pi + x * step) % overflow for pi, x in zip(point, reversed(axis), strict=True))
+            updated[new_point] = value
+        
+        self.p_colors = updated
+
+    def plot (self, dataset: Sequence[str], colors: Mapping[tuple[int], int], title: str):
+        """
+        Plots the given dataset using the given colors.
+        """
+        if (dims := len(self.axes)) == 2: fig, ax = plt.subplots()
+        else:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.set_zlabel('z')
+
+        coords, cols = [[] for _ in range(dims)], []
+        for point in self.explode(dataset):
+            cols.append(colors[point])
+
+            for sublist, pi in zip(coords, point, strict=True):
+                sublist.append(pi)
+
+        ax.set_title(title)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.scatter(*reversed(coords), c=cols)
+
+        return fig
+
+
 if __name__ == '__main__':
-    dims = 3
-    size = 8
+    dims2, dims3, size = 2, 3, 8
 
     # TESTS: draw all possible diagonals 
-    # rule = lambda x, y: x == y or x == size - 1 - y  # works only for dims = 2
-    rule = lambda x, y, z: (  # works only for dims = 3
+    rule2 = lambda x, y: x == y or x == size - 1 - y  # works only for dims = 2
+    rule3 = lambda x, y, z: (  # works only for dims = 3
         x == y == z or 
         x == y == size - 1 - z or 
         x == size - 1 - y == z or
         x == size - 1 - y == size - 1 - z
         )
 
-    qc, p, c = load_shape(dims, size, rule, show=False)
-    fig1 = plot(dims, qc, highlight=[1, 1, 1], show=False)
-
-    cycle_shift(qc, p[0], 3) # move 3 steps along the x-axis
-    cycle_shift(qc, p[1], 2) # move 2 steps along the y-axis
-    cycle_shift(qc, p[2], 1) # move 1 steps along the z-axis
+    # qc, p = encoding(dims2, size, rule2)
+    qc, p = encoding(dims3, size, rule3)
     
-    # coords are read in reverse order: [(z, ) y, x]
-    fig2 = plot(dims, qc, highlight=[2, 3, 4], show=False)
-
-    qc.draw('mpl')
-    plt.show()
+    with Plotter(qc, p) as pltr:
+        # move one step along the x/3=y/2=z axis
+        # ie mean 3 steps along the x-axis, 2 along the y-axis and 1 along the z-axis
+        pltr.shift([3, 2, 1], step=1)
+        
